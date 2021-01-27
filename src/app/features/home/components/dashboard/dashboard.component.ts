974a807e042a39742661ac4571a2b0ffcaac3b22
michael.paternina <mpaternina34@gmail.com>
2021-01-26 20:31:56 -0500

    SE REALIZAN AJUSTES VARIOS

--- src/app/features/home/components/dashboard/dashboard.component.ts
+++ src/app/features/home/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Route, Router } from '@angular/router';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +13,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
+  environmentIsProduction: string = "";
 
   constructor(changeDetectorRef: ChangeDetectorRef,
               media: MediaMatcher,
@@ -24,6 +26,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.mobileQuery.removeEventListener("change", this._mobileQueryListener);
+    this.environmentIsProduction = environment.urlBackend;
+    console.warn('Environment is PRODUCTION: ' + environment.production);
   }
 
 
